54108cfb0bd61a6e6d156c5f75846b8afc833414
basim-maf <basim.abusnaimeh@gmail.com>
2026-09-23 16:24:13 +0300

    update API URL to deployed backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://karar-complex-backend.onrender.com/api';
 
 export const api = axios.create({
   baseURL: API_URL,
